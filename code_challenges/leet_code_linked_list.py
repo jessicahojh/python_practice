cc89aefcd9a2fafdf4576b906d1cbd000ad61ae5
Jessica <jessica.ho.jh@gmail.com>
2019-12-13 12:12:54 -0800

    solved add LL and merge LL

---
+++ code_challenges/leet_code_linked_list.py
@@ -0,0 +1,79 @@
+# Add Two Numbers
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        head = l3 = ListNode(0)
+        carry = 0
+        
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val 
+                l2 = l2.next
+                
+            l3.val = carry % 10
+            carry = carry // 10
+            
+            if l1 or l2 or carry:
+                l3.next = ListNode(0)
+                l3 = l3.next
+                
+        return head
+
+###########################################################
+
+# Merge Two Linked Lists
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        p = l1
+        q = l2
+        s = None
+    
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p and q:
+            if p.val <= q.val:
+                s = p 
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s 
+        while p and q:
+            if p.val <= q.val:
+                s.next = p 
+                s = p 
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+        if not p:
+            s.next = q 
+        if not q:
+            s.next = p 
+        return new_head
